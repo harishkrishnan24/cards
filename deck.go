c17beb4c63608c2ae8b9c923799141cefd28b306
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-11 20:05:15 +0530

    Shuffle cards method

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -48,5 +49,17 @@ func newDeckFromFile(filename string) deck {
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
